66a6fffc52a5ceb97c19f8d7113bcdfb043172e3
Irina Chelariu <irina.clr27@gmail.com>
2023-02-19 15:39:18 +0200

    reference vs value types

---
+++ Program.cs
@@ -0,0 +1,45 @@
+﻿namespace languageBasicStuff
+{
+    internal class Program
+    {
+        #region value type example
+        //static int r = 27;
+        //static void f(int k)
+        //{
+        //    k += 5;
+        //    Console.WriteLine(k);
+        //}
+
+        //static int Main()
+        //{
+        //    f(r);
+        //    Console.WriteLine(r);
+        //    return 0;
+        //}
+        #endregion
+
+        #region reference type example
+        //static void f(int[] v)
+        //{
+        //    v[0] = 34;
+        //}
+
+        //static int Main()
+        //{
+        //    int[] v;
+        //    v = new int[10];
+        //    v[0] = 27;
+        //    f(v);
+        //    Console.WriteLine(v[0]);
+        //    return 0;
+        //}
+        #endregion
+
+        static int Main()
+        {
+            
+            return 0;
+        }
+
+    }
+}
